d5589384db2e56005b9ed7ed4181ad36c3e3e44b
derRiesenOtter <robin.ender@icloud.com>
2024-05-31 11:41:32 +0200

    Skript geupdated

--- Lebensmittelwarnung.py
+++ Lebensmittelwarnung.py
@@ -1,7 +1,16 @@
+import re
 import requests 
 from bs4 import BeautifulSoup 
 
 web_address = "https://www.lebensmittelwarnung.de/bvl-lmw-de/liste/alle/deutschlandweit/10/0"
 request_website = requests.get(web_address)
 
-bs_website = (request_website.text, "html.parser")
+bs_website = BeautifulSoup(request_website.text, "html.parser")
+
+types = bs_website.find_all("span", id=re.compile(r"e4pn"))
+dates = bs_website.find_all("span", id=re.compile(r"ecqn"))
+products = bs_website.find_all("span", id=re.compile(r"egqn"))
+company = bs_website.find_all("span", id=re.compile(r"ejqn"))
+cause = bs_website.find_all("span", id=re.compile(r"eyqn"))
+fed_states = bs_website.find_all("div", id=re.compile(r"e3qn"))
+
