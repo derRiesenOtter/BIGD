7f257ba6b7e98f2820b6ad440071d1c443e6be72
derRiesenOtter <robin.ender@icloud.com>
2024-05-31 12:38:02 +0200

    Skript weiter angepasst

--- Lebensmittelwarnung.py
+++ Lebensmittelwarnung.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup 
 import time
 import schedule
-import date
+import datetime
 
 def get_html_content():
     web_address = "https://www.lebensmittelwarnung.de/bvl-lmw-de/liste/alle/deutschlandweit/10/0"
@@ -24,12 +24,18 @@ def get_recent_content(html_content):
 
 # This function is supposed to take recent_content as input and check if there are new entries from the last day
 # It will return the number of new values
-def check_for_new_entries():
-    pass
+def check_for_new_entries(recent_content):
+    yesterday = datetime.date.today() - datetime.timedelta(1) 
+    n_new_entries = 0
+    for i in range(10):
+        if recent_content[i][1] == yesterday.strftime("%d.%m.%Y"):
+            n_new_entries += 1
+    return n_new_entries
 
 # This function will send the new values to our Stream
-def send_new_values():
-    pass
+def send_new_values(n_new_entries, recent_content):
+    if n_new_entries == 0:
+        return
 
 # This function will hold all tasks this script has to do every day
 def daily_task():
@@ -38,7 +44,9 @@ def daily_task():
 def main():
     html_content = get_html_content()
     recent_content = get_recent_content(html_content)
-    print(recent_content)
+    n_new_entries = check_for_new_entries(recent_content)
+    send_new_values(n_new_entries, recent_content)
+
     # This scheduler will take care of running the script daily
     # schedule.every().day.at("01:00").do(daily_task)
     # while True:
