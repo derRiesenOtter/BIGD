6fc48823e7b4f591445ef235cd61c39571f4c736
Amorgumena <katja.beck.9@gmx.de>
2024-06-13 11:33:22 +0200

    add cause_category to Lebensmittel.py

--- Lebensmittelwarnung.py
+++ Lebensmittelwarnung.py
@@ -33,6 +33,25 @@ def get_recent_content(html_content: BeautifulSoup) -> list[list[str]]:
     company = html_content.find_all("span", id=re.compile(r"ejqn"))
     cause = html_content.find_all("span", id=re.compile(r"eyqn"))
     fed_states = html_content.find_all("div", id=re.compile(r"e3qn"))
+
+    regex_microorganism = re.compile("(.Listeri.*)|(Salmonell.*)|(Patulin.*)|(.*(T|t)oxin.*)|(Pseudomon.*)|(Schimmel.*)|(Escherichia.*)|((M|m)ikro.*)|(Ba(c|z)ill.*)|(Hefe.*)")
+    regex_allergen = re.compile("(.*(A|a)llerg.*)|(.*nuss)|(Senf.*)|(Milch.*)")
+    regex_foreign_body = re.compile("(.*(F|f)remd.*)|(Glas.*)|(Metall.*)|(Kunststoff.*)|(Stein.*)")
+    regex_limit = re.compile("(.*(W|w)ert.*)|(.*(G|g)ehalt.*)|((R|r)ückst.*)|(.*(M|m)enge.*)|(Arznei.*)|(Nachweis.*)|((G|g)esund.*)|((G|g)esetz.*)|(krebs.*)|(Befund.*)|((G|g)ef(a|ä)hr.*)|(zugelassen.*)")
+    
+    cause_category = []
+    for i in cause:
+        if bool(regex_microorganism.search(i.text)):
+            cause_category.append('Mikroorganismen und Toxine')
+        elif bool(regex_allergen.search(i.text)):
+            cause_category.append('Allergene')
+        elif bool(regex_foreign_body.search(i.text)):
+            cause_category.append('Fremdkörper/-stoffe')
+        elif bool(regex_limit.search(i.text)):
+            cause_category.append('Grenzwertüberschreitung und Gesundheitsgefährdung')
+        else:
+            cause_category.append('Sonstiges')
+
     recent_content = []
     for i in range(10):
         recent_content.append(
@@ -42,6 +61,7 @@ def get_recent_content(html_content: BeautifulSoup) -> list[list[str]]:
                 products[i].text,
                 company[i].text,
                 cause[i].text,
+                cause_category[i]
                 fed_states[i].text,
             ]
         )
